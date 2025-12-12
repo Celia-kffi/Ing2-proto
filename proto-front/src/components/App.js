821b9647f43f750d1ffa4ebd3cda7621761e001f
Roumayssae <romyelkenz7@gmail.com>
2025-12-12 17:11:38 +0100

    test

--- proto-front/src/components/App.js
+++ proto-front/src/components/App.js
@@ -4,9 +4,9 @@ import '../styles/App.css';
 
 export default function App() {
   return (
+
       <div className="App">
           <h1>Hello World</h1>
-          <img src={logo} alt="logo"/>
 
       </div>
   );
