645327def2af26ea707f6ff4a46d4d34c3dbda3b
Roumayssae <romyelkenz7@gmail.com>
2026-01-18 19:25:25 +0100

    ajout des DTO d'itinéraire

---
+++ Triplan-back/src/main/java/triplan/back/dto/ItineraryRequest.java
@@ -0,0 +1,22 @@
+package triplan.back.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItineraryRequest {
+    private List<Long> activiteIds;
+    private Long pointDepartId;
+    private Long pointArriveeId;
+}
+
+
+
+
