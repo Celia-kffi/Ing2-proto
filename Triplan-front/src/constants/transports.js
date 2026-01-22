abfbc97fd82b89ad8b4cc0c00a1fcce4b6858b14
khadija idrissi <khadijaidrissi804@gmail.com>
2026-01-22 17:05:34 +0100

    ajustement des images / css

--- Triplan-front/src/constants/transports.js
+++ Triplan-front/src/constants/transports.js
@@ -1,9 +1,17 @@
+import marche from "../assets/marcher.png";
+import velo from "../assets/velo.png";
+import metro from "../assets/fret-ferroviaire.png";
+import voiture from "../assets/voiture.png";
+import voitureElec from "../assets/voiture-electrique.png";
+import bus from "../assets/autobus.png";
+import avion from "../assets/avion.png";
+
 export const MODES = [
-    { name: "Marche", img: "../assets/marche.png" },
-    { name: "Vélo", img: "../assets/velo.png" },
-    { name: "Métro", img: "../assets/fret-ferroviaire.png" },
-    { name: "Voiture essence", img: "../assets/voiture.png" },
-    { name: "Voiture électrique", img: "../assets/voiture-electrique.png" },
-    { name: "Bus urbain", img: "../assets/autobus.png" },
-    { name: "Avion court courrier", img: "../assets/avion.png" },
+    { name: "Marche", img: marche },
+    { name: "Vélo", img: velo },
+    { name: "Métro", img: metro },
+    { name: "Voiture essence", img: voiture },
+    { name: "Voiture électrique", img: voitureElec },
+    { name: "Bus urbain", img: bus },
+    { name: "Avion court courrier", img: avion },
 ];
