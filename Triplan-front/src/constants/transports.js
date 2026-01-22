77762d3bb8c078eaa20eee6de999e844a407ed68
khadija idrissi <khadijaidrissi804@gmail.com>
2026-01-22 11:11:20 +0100

    amélioration front

--- Triplan-front/src/constants/transports.js
+++ Triplan-front/src/constants/transports.js
@@ -1,9 +1,9 @@
 export const MODES = [
-    { name: "Marche", img: "/assets/marche.png" },
-    { name: "Vélo", img: "/assets/velo.png" },
-    { name: "Métro", img: "/assets/metro.png" },
-    { name: "Voiture essence", img: "/assets/voiture.png" },
-    { name: "Voiture électrique", img: "/assets/voiture_elec.png" },
-    { name: "Bus urbain", img: "/assets/bus.png" },
-    { name: "Avion court courrier", img: "/assets/avion.png" },
+    { name: "Marche", img: "../assets/marche.png" },
+    { name: "Vélo", img: "../assets/velo.png" },
+    { name: "Métro", img: "../assets/fret-ferroviaire.png" },
+    { name: "Voiture essence", img: "../assets/voiture.png" },
+    { name: "Voiture électrique", img: "../assets/voiture-electrique.png" },
+    { name: "Bus urbain", img: "../assets/autobus.png" },
+    { name: "Avion court courrier", img: "../assets/avion.png" },
 ];
