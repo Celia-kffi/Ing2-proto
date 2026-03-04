db712bbb32d3b996af8b8bea29b7b3cf7947ad1b
Celia-kffi <120136130+Celia-kffi@users.noreply.github.com>
2026-03-04 16:22:00 +0100

    Update CORS policy to allow all origins

--- Triplan-back/src/main/java/triplan/back/controllers/HebergementController.java
+++ Triplan-back/src/main/java/triplan/back/controllers/HebergementController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/hebergements")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class HebergementController {
 
     private final HebergementRepository hebergementRepository;
