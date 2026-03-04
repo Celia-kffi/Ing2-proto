cd39da520c1e8af089cc84bdb73313d4416af01c
Celia-kffi <120136130+Celia-kffi@users.noreply.github.com>
2026-03-04 16:38:25 +0100

    Update CORS origin in RecommendationController

--- Triplan-back/src/main/java/triplan/back/controllers/RecommendationController.java
+++ Triplan-back/src/main/java/triplan/back/controllers/RecommendationController.java
@@ -64,7 +64,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/recommandations")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://172.31.253.128:3000")
 public class RecommendationController {
 
     private final RecommendationService recommendationService;
