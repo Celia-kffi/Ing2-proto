06fc801b4f5aea461638ab5ee827e593896680b7
Roumayssae <romyelkenz7@gmail.com>
2026-03-05 13:56:10 +0100

    désactivation du warning eslint

--- Triplan-front/src/components/OptimalItinerary.jsx
+++ Triplan-front/src/components/OptimalItinerary.jsx
@@ -24,6 +24,7 @@ function OptimalItinerary() {
 
     useEffect(() => {
         loadVilleEtActivites();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const loadVilleEtActivites = async () => {
