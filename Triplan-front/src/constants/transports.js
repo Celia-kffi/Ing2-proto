dd95fe55a9347f37d00157ee99195d559e380a0f
Celia-kffi <120136130+Celia-kffi@users.noreply.github.com>
2026-04-03 10:02:30 +0200

    Remove unused avion image import
    Removed unused import for avion image.

--- Triplan-front/src/constants/transports.js
+++ Triplan-front/src/constants/transports.js
@@ -4,7 +4,7 @@ import metro from "../assets/fret-ferroviaire.png";
 import voiture from "../assets/voiture.png";
 import voitureElec from "../assets/voiture-electrique.png";
 import bus from "../assets/autobus.png";
-import avion from "../assets/avion.png";
+
 
 export const MODES = [
     { name: "Marche", img: marche },
